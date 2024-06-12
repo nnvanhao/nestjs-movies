33b9611c6d7d0a2b017c78b8e9f5f98c54eb98d7
nnvanhao <nnvanhao@gmail.com>
2024-06-12 14:43:02 +0700

    update server url

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -26,9 +26,8 @@ export class HealthController {
   @Get()
   @HealthCheck()
   async check() {
-    const ipAddress = this.getServerIp();
     return this.health.check([
-      () => this.http.pingCheck('nest-app', `http://${ipAddress}/v1/`),
+      () => this.http.pingCheck('nest-app', `${process.env.SERVER_URL}/v1/`),
       () => this.db.pingCheck('database', this.prismaService),
       () =>
         this.disk.checkStorage('storage', {
@@ -38,17 +37,5 @@ export class HealthController {
       () => this.memory.checkHeap('memory_heap', 150 * 1024 * 1024),
       () => this.memory.checkRSS('memory_rss', 150 * 1024 * 1024),
     ]);
-  }
-
-  private getServerIp(): string {
-    const interfaces = os.networkInterfaces();
-    for (const name of Object.keys(interfaces)) {
-      for (const iface of interfaces[name]) {
-        if (iface.family === 'IPv4' && !iface.internal) {
-          return iface.address;
-        }
-      }
-    }
-    return 'localhost'; // fallback to localhost if no external IP is found
   }
 }
